f342ef9b1d4e02a44e804edf5ab32e47b9863d4f
javierParamo <javierParamo@javierParamo>
2017-06-14 13:39:13 +0200

    limpieza en gui

--- Aplicacion/Thoth-Web/src/src/client/gui/MyComposite.java
+++
@@ -1,9 +0,0 @@
-package src.client.gui;
-
-import com.google.gwt.user.client.ui.Composite;
-
-
-public abstract class MyComposite extends Composite {
-	//public abstract void handleEvent(DataEvent event);
-	public abstract void handleError(String error);
-}
